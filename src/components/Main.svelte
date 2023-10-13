466844ef2f8bfc470c201c53162eb3b491ccc1d6
Keith Flynn <flynn.keith@gmail.com>
2023-10-12 21:59:51 -0400

    Add styling for Main.svelte
    Add styles and some barebones content for visualization.

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -3,7 +3,13 @@
 </script>
 
 <main class="flex flex-col flex-1 p-4">
-  <section id="introPage">
+  <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
+    <div class="flex flex-col lg:justify-center text-center lf:text-left gap-6 md:gap-8 lg:gap-10">
+      <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+        Hi! I'm <span class="poppins text-violet-400">Keith</span> Flynn <br/>Front End 
+        <span class="poppins text-violet-400">Developer</span>
+      </h2>
+    </div>
 
   </section>
 </main>
