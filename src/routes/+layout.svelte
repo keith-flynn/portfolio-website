2bbd1ba14da6b78b121b0d62a6bef797b80d62dd
Keith Flynn <flynn.keith@gmail.com>
2023-10-14 11:52:23 -0400

    Minor layout tweaks

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,20 +12,19 @@
   }
 </script>
 
-<div class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
+<div class="container relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
   <div class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " + 
     (y > 0 
       ? "opacity-full pointer-events-auto"
       : "pointer-events-none opacity-0"
     )}>
-    <button on:click={goTop} class="ml-auto rounded-full aspect-square bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer aspect-square grid place-items-center">
+    <button on:click={goTop} class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer aspect-square grid place-items-center">
       <i class="fa-solid fa-arrow-up" />
     </button>
   </div>
-  <Header y={y}/>
+  <Header {y} {innerHeight}/>
   <slot />
   <Footer />
 </div>
 
 <svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
-<slot />
