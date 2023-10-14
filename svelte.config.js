9820e1642d2065406ca835563b4791134164fa6b
Keith Flynn <flynn.keith@gmail.com>
2023-10-13 21:05:35 -0400

    paths tweak

--- svelte.config.js
+++ svelte.config.js
@@ -3,11 +3,9 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
-    paths: {
-      base: dev ? '' : process.env.BASE_PATH,
-    }
+    adapter: adapter()
   },
   preprocess: vitePreprocess()
 };
+config.paths = { base: process.argv.includes('dev') ? '' : process.env.BASE_PATH }
 export default config;
