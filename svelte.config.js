bb5abc7755bcb13dc7493b012dbcb50352acc289
Keith Flynn <flynn.keith@gmail.com>
2023-10-13 20:40:27 -0400

    Deployment test

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    paths: {
+      base: dev ? '' : process.env.BASE_PATH,
+    }
   },
   preprocess: vitePreprocess()
 };
