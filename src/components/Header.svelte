e335290dc3cee21e7dc2b6e97333b31dc69afe6d
Keith Flynn <flynn.keith@gmail.com>
2023-10-14 22:08:15 -0400

    Update Readme and remove duplicate workflow

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -38,14 +38,14 @@
           <h4 class="relative z-9">Login</h4>
       </button> -->
       <a href="#contact">
-      <button
-          class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
-      >
-          <div
-              class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
-          />
-          <h4 class="relative z-9">Get in touch</h4>
-      </button>
+        <button
+            class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
+        >
+            <div
+                class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+            />
+            <h4 class="relative z-9">Get in touch</h4>
+        </button>
       </a>
   </div>
 </header>
